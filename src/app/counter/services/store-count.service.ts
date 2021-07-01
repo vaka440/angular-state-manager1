9caded3f8e4f2755745ee4242853708a0a760f05
vaka440 <82819283+vaka440@users.noreply.github.com>
2021-07-01 13:10:12 +0200

    Update store-count.service.ts

--- src/app/counter/services/store-count.service.ts
+++ src/app/counter/services/store-count.service.ts
@@ -5,28 +5,30 @@ import { ICount } from '../models/i-count';
 @Injectable({
   providedIn: 'root',
 })
-export class StoreCountService extends BaseStoreService<ICount> {
-  getTypeName(): string {
-    // pour le mode DEV, toujours renvoyer le type
-    return 'ICount';
+export class StoreCountService extends BaseStoreService<ICount> {   // add here the model
+  getTypeName(): string { 
+    return 'ICount';    // for DEV mode, add here the name of the model
   }
 
   getInitial(): ICount[] {
-    // ici, il faut toujours retourner un tableau de quelquechose. Dans ce service c'est un tableau de ICount
-
-    return [{ value: 0 } as ICount];
+    //   
+    // initialize here something empty (an array, an object...)
+    // or use an api to initialize with the received data 
+    //
+    return [{ value: 0 } as ICount];    // value will contain the current counter value
   }
 
-  // incrémentation d'une valeur
-  // comme c'est une action particulière qui ne fait pas partie des actions de bases comme (ADD, REMOVE, UPDATE...)
-  // on surcharge en écrivant ici la fonction
+  // incrementing a value
+  // as it is a particular action which is not part of the basic actions like (ADD, REMOVE, UPDATE...)
+  //
+  // we write here the particular function, for our counter, the increment function
   inc() {
-    const obj = this.values[0] as ICount; // comme ce n'est pas une liste, c'est un tableau avec un seul index qui est égal à 0
+    const obj = this.values[0] as ICount; // as it is not a list, it is an array with a single index which is equal to 0
     if (obj) {
-      obj.value++; // on effectue l'action ici, on incrémente
+      obj.value++;              // incrementing is carried out
 
-      this.update(obj, 'INC'); // on fait appelle à update de la classe abstraite : BaseStoreService
-      // en précisant le nom de l'action : 'INC'
+      this.update(obj, 'INC');  // we call update, a function of the abstract class
+                                // a particular action therefore a particular name : 'INC'
     }
   }
 }
